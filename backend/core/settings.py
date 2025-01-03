bdbbcd5bcd13d563cf19e04d3dccab4d49f8352e
Amanmyrat <amansarahs@gmail.com>
2025-01-03 17:50:52 +0500

    Done with initial backend setup

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*', ]
+ALLOWED_HOSTS = ['*', 'backend.airporttransfer.transfertakip.com']
 
 AUTH_USER_MODEL = 'accounts.Account'
 
