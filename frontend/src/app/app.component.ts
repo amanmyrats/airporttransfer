e427dfa9b402059d387dfd383600c46d7391e843
Amanmyrat <amansarahs@gmail.com>
2025-06-13 19:53:28 +0500

    Removed old cookie consent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CookieConsentComponent } from './components/cookie-consent/cookie-consent.component';
 
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
-    CookieConsentComponent, 
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
