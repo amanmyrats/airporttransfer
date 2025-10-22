e9b6868bc6117e41ee3679409c8e2ff1c0fbaad1
Amanmyrat <amansarahs@gmail.com>
2025-10-22 17:18:20 +0500

    Changed home page booking-form's bg dark

--- frontend/src/app/components/footer-placeholder/footer-placeholder.component.ts
+++ frontend/src/app/components/footer-placeholder/footer-placeholder.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { SOCIAL_ICONS } from '../../constants/social.constants';
+import { NAVBAR_MENU } from '../../constants/navbar-menu.constants';
+import { LanguageService } from '../../services/language.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-footer-placeholder',
@@ -6,6 +10,119 @@ import { Component } from '@angular/core';
   templateUrl: './footer-placeholder.component.html',
   styleUrl: './footer-placeholder.component.scss'
 })
-export class FooterPlaceholderComponent {
+export class FooterPlaceholderComponent  implements OnInit {
+  socialIcons = SOCIAL_ICONS;
+  navbarMenu = NAVBAR_MENU;
+  private languageService!: LanguageService;
+  
+  currentLanguage: any = {
+    code: 'en',
+    name: 'English',
+    flag: 'flags/gb.svg',
+  };
 
+  constructor(private route: ActivatedRoute) {
+      if (typeof window !== 'undefined') {
+        this.languageService = inject(LanguageService);}
+  }
+
+  ngOnInit(): void {
+    const languageCode = this.route.snapshot.data['language'] || 'en';
+    this.currentLanguage.code = languageCode;
+  }
+
+  onLanguageSelect(langCode: any): void {
+    this.languageService.setLanguage(langCode, true)
+  }
+
+  getTranslation(key: string): string {
+    return this.translations[key]?.[this.currentLanguage.code] || key;
+  }
+
+  translations: any = {
+    home: {
+      en: 'Homepage',
+      de: 'Startseite',
+      ru: 'Главная страница',
+      tr: 'Anasayfa',
+    }, 
+    contactus: {
+      en: 'Contact Us',
+      de: 'Kontaktiere uns',
+      tr: 'Bize Ulaşın',
+      ru: 'Свяжитесь с нами',
+    },
+    quicklinks: {
+      en: 'Quick Links',
+      de: 'Schnelle Links',
+      tr: 'Hızlı Bağlantılar',
+      ru: 'Быстрые ссылки',
+    },
+    followus: {
+      en: 'Follow Us',
+      de: 'Folge uns',
+      tr: 'Bizi takip et',
+      ru: 'Следите за нами',
+    },
+    aboutus: {
+      en: 'About Us',
+      de: 'Über uns',
+      tr: 'Hakkımızda',
+      ru: 'О нас',
+    },
+    ourservices: {
+      en: 'Our Services',
+      de: 'Unsere Leistungen',
+      tr: 'Hizmetlerimiz',
+      ru: 'Наши услуги',
+    },
+    blog: {
+      en: 'Blog',
+      de: 'Blog',
+      tr: 'Blog',
+      ru: 'Блог',
+    },
+    newBlog: {
+      en: 'New Blog',
+      de: 'Neuer Blog',
+      tr: 'Yeni Blog',
+      ru: 'Новый блог',
+    },
+    contact: {
+      en: 'Contact',
+      de: 'Kontakt',
+      tr: 'İletişim',
+      ru: 'Контакт',
+    },
+    gallery: {
+      en: 'Gallery',
+      de: 'Galerie',
+      tr: 'Galeri',
+      ru: 'Галерея',
+    }, 
+    privacyPolicy: {
+      en: 'Privacy Policy',
+      de: 'Datenschutz-Bestimmungen',
+      tr: 'Gizlilik Politikası',
+      ru: 'Политика конфиденциальности',
+    },
+    termsOfService: {
+      en: 'Terms of Service',
+      de: 'Nutzungsbedingungen',
+      tr: 'Hizmet Şartları',
+      ru: 'Условия обслуживания',
+    },
+    allRightsReserved: {
+      en: 'All Rights Reserved.',
+      de: 'Alle Rechte vorbehalten.',
+      tr: 'Tüm Hakları Saklıdır.',
+      ru: 'Все права защищены.',
+    },
+    allLanguages: {
+      en: 'Languages',
+      de: 'Sprachen',
+      tr: 'Diller',
+      ru: 'Языки',
+    }
+  };
 }
